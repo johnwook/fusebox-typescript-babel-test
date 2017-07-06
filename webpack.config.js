be117b748d6236a833e92ea3eb835d5c713557c0
Johnwook Choi <johnwook.choi@gmail.com>
2017-07-06 11:20:46 +0900

    Add webpack integration
    - use ts-loader with babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var babelOptions = {
+  sourceMaps: true,
+  presets: ["es2015"],
+  plugins: [["transform-react-jsx"]]
+};
+
+module.exports = {
+  entry: {
+    main: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, './out'),
+    filename: 'tsWithWebpack.js',
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: babelOptions
+        },
+        {
+          loader: 'ts-loader'
+        }
+      ]
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+};
